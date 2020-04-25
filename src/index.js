d94ea7a81a8904c82442e0bc52e2866fbe8ac2ba
Wolmer Martins <wolmermartins@hotmail.com>
2020-04-25 06:55:22 -0300

    ENH: Initializes the database setup to create DB and table

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 'use strict'
 
+const database = require('./config/database')
 const server = require('./config/server')
 const config = require('./config/config')
 const routes = require('./router')
+
+database(config.db)
 
 server.use(routes)
 
