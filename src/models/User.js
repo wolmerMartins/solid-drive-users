987c9d5e37aeec32da47b8fa660bfbed057e9d65
Wolmer Martins <wolmermartins@hotmail.com>
2020-04-25 06:54:03 -0300

    ENH: Creates the User model

---
+++ src/models/User.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const { DataTypes } = require('sequelize')
+
+const setup = async (sequelize) => {
+  const User = sequelize.define('User', {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    email: {
+      type: DataTypes.STRING(120),
+      allowNull: false,
+      unique: true
+    },
+    username: {
+      type: DataTypes.STRING(30),
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING(22),
+      allowNull: false
+    }
+  }, {
+    tableName: 'users'
+  })
+
+  await User.sync()
+
+  exports.User = User
+}
+
+module.exports = setup
