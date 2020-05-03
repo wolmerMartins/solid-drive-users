6e9c3241f55801912cac6b857a955309430832fe
Wolmer Martins <wolmermartins@hotmail.com>
2020-05-03 08:14:38 -0300

    TST: validationErrorSchema

---
+++ src/controllers/test/validationErrorSchema.test.js
@@ -0,0 +1,87 @@
+'use strict'
+
+const chai = require('chai')
+const expect = chai.expect
+
+const validationErrorSchema = require('../validationErrorSchema')
+
+const {
+  MESSAGES,
+  MAX_LENGTH_CODE,
+  NOT_CONTAIN_LETTERS,
+  NOT_CONTAIN_NUMBERS,
+  MISSING_PARAMETER_CODE,
+  INVALID_PARAMETER_CODE,
+  NOT_CONTAIN_SPECIAL_CHARACTER
+} = require('../../constants')
+
+describe('validationErrorSchema', () => {
+  const missedParameters = ['username', 'email']
+
+  describe('missing parameter', () => {
+    it('Should return a MISSING PARAMETER error message', () => {
+      const { code, message } = validationErrorSchema(MISSING_PARAMETER_CODE, missedParameters)
+
+      expect(code)
+        .to
+        .equal(MISSING_PARAMETER_CODE)
+
+      expect(message)
+        .to
+        .equal(`${MESSAGES[MISSING_PARAMETER_CODE]}: ${missedParameters.join(', ')}`)
+    })
+  })
+
+  describe('invalid parameter', () => {
+    it('Should return an INVALID PARAMETER error message', () => {
+      const { code, message } = validationErrorSchema(INVALID_PARAMETER_CODE, 'email')
+
+      expect(code)
+        .to
+        .equal(INVALID_PARAMETER_CODE)
+
+      expect(message)
+        .to
+        .equal(`${MESSAGES[INVALID_PARAMETER_CODE]}: email`)
+    })
+
+    it('Should return an INVALID PARAMETER error message with a specific message', () => {
+      const { code, message } = validationErrorSchema(INVALID_PARAMETER_CODE, 'password', NOT_CONTAIN_LETTERS)
+
+      expect(code)
+        .to
+        .equal(INVALID_PARAMETER_CODE)
+
+      expect(message)
+        .to
+        .equal(`${MESSAGES[INVALID_PARAMETER_CODE]}: password: ${MESSAGES[NOT_CONTAIN_LETTERS]}`)
+    })
+
+    it('Should return an INVALID PARAMETER error message with two specific messages', () => {
+      const {
+        code,
+        message
+      } = validationErrorSchema(INVALID_PARAMETER_CODE, 'password', [NOT_CONTAIN_NUMBERS, NOT_CONTAIN_SPECIAL_CHARACTER])
+
+      expect(code)
+        .to
+        .equal(INVALID_PARAMETER_CODE)
+
+      expect(message)
+        .to
+        .equal(`${MESSAGES[INVALID_PARAMETER_CODE]}: password: ${MESSAGES[NOT_CONTAIN_NUMBERS]}, ${MESSAGES[NOT_CONTAIN_SPECIAL_CHARACTER]}`)
+    })
+
+    it('Should return an INVALID PARAMETER error message with a specific message and a required parameter', () => {
+      const { code, message } = validationErrorSchema(INVALID_PARAMETER_CODE, 'username', MAX_LENGTH_CODE, 30)
+
+      expect(code)
+        .to
+        .equal(INVALID_PARAMETER_CODE)
+
+      expect(message)
+        .to
+        .equal(`${MESSAGES[INVALID_PARAMETER_CODE]}: username: ${MESSAGES[MAX_LENGTH_CODE]} 30`)
+    })
+  })
+})
