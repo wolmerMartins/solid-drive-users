3c66e2428871d4e1df4ca7c280033a7257ec7580
Wolmer Martins <wolmermartins@hotmail.com>
2020-05-06 07:56:16 -0300

    REF: Changes the config.js to use environment variables

--- src/config/config.js
+++ src/config/config.js
@@ -1,17 +1,28 @@
 'use strict'
 
+require('custom-env').env(true)
+
+const {
+  PORT,
+  DB_NAME,
+  DB_USER,
+  DB_HOST,
+  DB_PASSWORD,
+  PUSHPIN_PUBLISH
+} = process.env
+
 const config = {
   app: {
-    port: 3000
+    port: PORT
   },
   pushpin: {
-    publishUrl: 'http://pushpin:5561/publish'
+    publishUrl: PUSHPIN_PUBLISH
   },
   db: {
-    database: 'solid_drive',
-    user: 'solid',
-    password: 'solid',
-    host: 'db',
+    database: DB_NAME,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    host: DB_HOST,
     dialect: 'mysql'
   }
 }
