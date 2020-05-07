939cfc3b37242175d4af02a30658b2fa979670ee
Wolmer Martins <wolmermartins@hotmail.com>
2020-05-07 07:59:22 -0300

    ENH: Adds logger to index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ const database = require('./config/database')
 const server = require('./config/server')
 const config = require('./config/config')
 const routes = require('./router')
+const logger = require('./logger')
 
 database(config.db)
 
 server.use(routes)
 
 server.listen(config.app.port, () => {
-  console.log(`API listens at the port ${config.app.port}`)
+  logger.info(`API listens at the port ${config.app.port}`)
 })
