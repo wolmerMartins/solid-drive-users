ac829f85445cb288aa7e81d5490203ae5fb49493
Wolmer Martins <wolmermartins@hotmail.com>
2020-05-07 08:07:36 -0300

    ENH: Adds logs to controller user

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -2,7 +2,10 @@
 
 const pushpin = require('./pushpin')
 const model = require('../models/User')
+const controllerLogger = require('./logger')
 const { hashPassword } = require('./password')
+
+const logger = controllerLogger.child({ module: 'user' })
 
 const userController = {
   create: async (body, channel) => {
@@ -15,11 +18,15 @@ const userController = {
     try {
       const user = await model.createUser(userData)
 
+      logger.info({ user }, 'User created successfully')
+
       pushpin.publish.response({
         data: { user, using: 'pushpin' },
         channel
       })
     } catch(error) {
+      logger.error({ error }, 'An error has occurred trying to create a new User')
+
       pushpin.publish.response({
         data: { error, using: 'pushpin' },
         channel
