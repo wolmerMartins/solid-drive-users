449861f6559e7891ad1b493f78e1e73555d82411
Wolmer Martins <wolmermartins@hotmail.com>
2020-05-07 08:06:03 -0300

    ENH: Adds logs to config database

--- src/config/database.js
+++ src/config/database.js
@@ -3,6 +3,9 @@
 const { Sequelize } = require('sequelize')
 
 const model = require('../models/User')
+const configLogger = require('./logger')
+
+const logger = configLogger.child({ module: 'database' })
 
 let sequelize
 
@@ -26,9 +29,9 @@ const setup = async ({
   try {
     await sequelize.authenticate()
     await model(sequelize)
-    console.log('Database connected and user table created successfully.')
-  } catch(err) {
-    console.log('Unable to connect to the database:', err)
+    logger.info('Database connected and user table created successfully')
+  } catch(error) {
+    logger.error({ error }, 'Unable to connect to the database')
   }
 }
 
