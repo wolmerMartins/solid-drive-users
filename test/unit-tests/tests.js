f055cbceb482ada52aa2964f5459c37b5a171bd2
Wolmer Martins <wolmermartins@hotmail.com>
2020-05-10 07:45:41 -0300

    TST: Changes the test suite to run on CI

--- test/unit-tests/tests.js
+++ test/unit-tests/tests.js
@@ -1,14 +1,12 @@
 'use strict'
 
-describe('unit tests', () => {
-  describe('controllers', () => {
-    require('../../src/controllers/test/pushpin.test')
-    require('../../src/controllers/test/password.test')
-    require('../../src/controllers/test/validateUser.test')
-    require('../../src/controllers/test/validationErrorSchema.test')
-  })
+describe('controllers', () => {
+  require('../../src/controllers/test/pushpin.test')
+  require('../../src/controllers/test/password.test')
+  require('../../src/controllers/test/validateUser.test')
+  require('../../src/controllers/test/validationErrorSchema.test')
+})
 
-  describe('models', () => {
-    require('../../src/models/test/User.test')
-  })
+describe('models', () => {
+  require('../../src/models/test/User.test')
 })
