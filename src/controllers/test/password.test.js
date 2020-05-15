ba357ff962c73e0eb9ca62e03d3d269b2239b2c8
Wolmer Martins <wolmermartins@hotmail.com>
2020-05-15 07:13:05 -0300

    ENH: Creates the verifyPassword function

--- src/controllers/test/password.test.js
+++ src/controllers/test/password.test.js
@@ -3,10 +3,12 @@
 const chai = require('chai')
 const expect = chai.expect
 
-const { hashPassword } = require('../password')
+const { hashPassword, verifyPassword } = require('../password')
 
 describe('password', () => {
   const password = 'testasdas1135234#&*&$%'
+  const passwordFail = 'testasdas#&*&$%1135234'
+  const hashedPassword = 'nPlw7eBu5q32fzE+RTC/8Q==$GAT6/y8nClLIuvBasqDhxAC6/YgPEpe0xJfVk4uH6ayMRMX7NgbSXl8bDcvZ1quc/LMuX9BIkB4hw4XedMsRig=='
 
   describe('hashPassword', () => {
     it('Should create a hash from the given password', () => {
@@ -20,6 +22,34 @@ describe('password', () => {
 
       expect(hashPass)
         .to.not.be.equal(password)
+    })
+  })
+
+  describe('verifyPassword', () => {
+    it('Should verify if the passwords match and return true', () => {
+      const body = { password }
+      const user = { password: hashedPassword }
+
+      const passOk = verifyPassword({ body, user })
+
+      expect(passOk)
+        .to.be.a('boolean')
+
+      expect(passOk)
+        .to.be.true
+    })
+
+    it('Should verify if the passwords match and return false', () => {
+      const body = { password: passwordFail }
+      const user = { password: hashedPassword }
+
+      const passFail = verifyPassword({ body, user })
+
+      expect(passFail)
+        .to.be.a('boolean')
+
+      expect(passFail)
+        .to.be.false
     })
   })
 })
