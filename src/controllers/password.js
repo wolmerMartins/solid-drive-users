dbc739a00e228be4ed331785235202a9d39a0b7c
Wolmer Martins <wolmermartins@hotmail.com>
2020-05-15 07:13:05 -0300

    ENH: Creates the verifyPassword function

--- src/controllers/password.js
+++ src/controllers/password.js
@@ -2,11 +2,25 @@
 
 const crypto = require('crypto')
 
+const generateHash = ({ salt, password }) => crypto
+  .createHmac('sha512', salt).update(password).digest('base64')
+
 const hashPassword = ({ password }) => {
   const salt = crypto.randomBytes(16).toString('base64')
-  const hash = crypto.createHmac('sha512', salt).update(password).digest('base64')
+  const hash = generateHash({ salt, password })
 
   return `${salt}$${hash}`
 }
 
+const verifyPassword = ({ body, user }) => {
+  const { password: bodyPassword } = body
+  const { password: userPassword } = user
+
+  const [salt, passwordHash] = userPassword.split('$')
+  const hash = generateHash({ salt, password: bodyPassword })
+
+  return (passwordHash === hash)
+}
+
 exports.hashPassword = hashPassword
+exports.verifyPassword = verifyPassword
