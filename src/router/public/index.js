997d8b905a45173ce9f1f2facd46312cae0a9b96
Wolmer Martins <wolmermartins@hotmail.com>
2020-05-16 08:48:51 -0300

    ENH: Creates the public auth route

--- src/router/public/index.js
+++ src/router/public/index.js
@@ -12,8 +12,27 @@ const {
   validateEmail,
   validatePassword
 } = require('../../controllers/validateUser')
+const {
+  checkIfUserExists,
+  checkIfPasswordMatch,
+  validateLoginRequiredParameters
+} = require('../../controllers/validateLogin')
 
 const logger = routerLogger.child({ module: 'public' })
+
+const errorResponse = ({ res, error }) => {
+  const { message, code, statusCode } = error
+
+  res.status(statusCode ?? 400).json({ message, code })
+}
+
+const signToPushpin = ({ res, channelName }) => {
+  const channel = pushpin.getChannel({ channelName })
+
+  pushpin.sign.response({ res, channel })
+
+  return channel
+}
 
 router.post('/', async (req, res) => {
   const { body } = req
@@ -25,18 +44,34 @@ router.post('/', async (req, res) => {
     validatePassword(body)
 
     const { username } = body
-  
-    const channel = pushpin.getChannel({ channelName: username })
-  
-    pushpin.sign.response({ res, channel })
+
+    const channel = signToPushpin({ res, channelName: username })
 
     userController.create(body, channel)
   } catch(error) {
-    const { message, code, statusCode } = error
+    logger.error({ error }, 'An error has occurred on create user')
 
-    logger.error({ error }, 'An error has occurred on /POST')
+    errorResponse({ res, error })
+  }
+})
 
-    return res.status(statusCode ?? 400).json({ message, code })
+router.post('/auth', async (req, res) => {
+  const { body } = req
+
+  try {
+    validateLoginRequiredParameters(body)
+    const user = await checkIfUserExists(body)
+    checkIfPasswordMatch({ body, user })
+
+    const { login } = body
+
+    const channel = signToPushpin({ res, channelName: login })
+
+    userController.auth(user, channel)
+  } catch(error) {
+    logger.error({ error }, 'An error has occurred on auth user')
+
+    errorResponse({ res, error })
   }
 })
 
