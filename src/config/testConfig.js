358001f0c1106f2384e8cfa770beca87f183a664
Wolmer Martins <wolmermartins@hotmail.com>
2020-05-20 07:46:49 -0300

    ENH: Creates the testConfig file to mock environment variables to test

---
+++ src/config/testConfig.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const testConfig = {
+  TST_PORT: 3000,
+  TST_PUSHPIN_PUBLISH: 'http://pushpin.com/publish',
+  TST_DB_NAME: 'dbName',
+  TST_DB_USER: 'dbUser',
+  TST_DB_PASSWORD: 'dbPassword',
+  TST_DB_HOST: 'dbHost',
+  TST_REDIS_DB: 'redisdb',
+  TST_REDIS_HOST: 'redisHost',
+  TST_REDIS_PORT: 'redisPort',
+  TST_JWT_SECRET: 'jwtsecret'
+}
+
+module.exports = testConfig
