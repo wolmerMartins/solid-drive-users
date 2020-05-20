c7ce93812adcb80a0887631c5d682d6f4b31d5db
Wolmer Martins <wolmermartins@hotmail.com>
2020-05-20 08:12:37 -0300

    ENH: Initializes redis client

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@
 const database = require('./config/database')
 const server = require('./config/server')
 const config = require('./config/config')
+const redis = require('./config/redis')
 const routes = require('./router')
 const logger = require('./logger')
 
 database(config.db)
+redis(config.redis)
 
 server.use(routes)
 
