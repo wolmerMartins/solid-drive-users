61fc730f197048c20ffcee6485aecc2c0b0cdac5
Wolmer Martins <wolmermartins@hotmail.com>
2020-05-25 08:00:40 -0300

    ENH: Creates the private router

---
+++ src/router/private/index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+  res.status(200).json({ get: 'users' })
+})
+
+module.exports = router
