c2d93745762daba1f43b85d4f5a955c4b10f34e5
Wolmer Martins <wolmermartins@hotmail.com>
2020-05-26 07:13:21 -0300

    ENH: Uses the auth middleware and the private router

--- src/router/index.js
+++ src/router/index.js
@@ -4,8 +4,12 @@ const express = require('express')
 const router = express.Router()
 
 const publicRoutes = require('./public')
+const privateRoutes = require('./private')
+const auth = require('../middlewares/auth')
 
 router.use('/api/v1/users', publicRoutes)
+router.use(auth)
+router.use('/api/v1/users', privateRoutes)
 
 router.use((req, res) => res
   .status(404)
