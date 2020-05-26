c9008c0d26fb31b63652a1bddbf344d341b12dcb
Wolmer Martins <wolmermartins@hotmail.com>
2020-05-26 07:09:57 -0300

    ENH: Creates the getCookies utils function

--- src/controllers/utils.js
+++ src/controllers/utils.js
@@ -6,9 +6,25 @@ const {
   USERNAME_FIELD
 } = require('../constants')
 
+const getCookies = ({ cookie }) => {
+  const cookiesObj = new Map()
+  if (!cookie) return cookiesObj
+
+  const cookies = cookie.split('; ')
+  const cookiesLimit = cookies.length
+
+  for (let ck = 0; ck < cookiesLimit; ck++) {
+    const [key, value] = cookies[ck].split('=')
+    cookiesObj.set(key, value)
+  }
+
+  return cookiesObj
+}
+
 const getUserKeyFromLogin = ({ login }) => {
   if (login.includes(AT_SYMBOL)) return { [EMAIL_FIELD]: login }
   return { [USERNAME_FIELD]: login }
 }
 
 exports.getUserKeyFromLogin = getUserKeyFromLogin
+exports.getCookies = getCookies
