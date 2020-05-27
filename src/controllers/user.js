6005f2e18a7930682b61d3758d0b72859bcd6fd6
Wolmer Martins <wolmermartins@hotmail.com>
2020-05-27 08:21:40 -0300

    ENH: Creates the update user

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,14 +1,28 @@
 'use strict'
 
+const _ = require('lodash')
+
 const model = require('../models/User')
 const session = require('../models/Session')
 
-const { hashPassword } = require('./password')
 const controllerLogger = require('./logger')
-const { generateToken } = require('./jwt')
 const pushpin = require('./pushpin')
+const { hashPassword } = require('./password')
+const { generateToken } = require('./jwt')
+const { checkIfUserExists } = require('./validateLogin')
+const {
+  validateEmail,
+  validateUsername,
+  validatePassword
+} = require('./validateUser')
 
 const logger = controllerLogger.child({ module: 'user' })
+
+const sendErrorResponse = ({ error, channel }) => pushpin
+  .publish.response({
+    data: { error },
+    channel
+  })
 
 const userController = {
   create: async (body, channel) => {
@@ -30,10 +44,7 @@ const userController = {
     } catch(error) {
       logger.error({ error }, 'An error has occurred trying to create a new User')
 
-      pushpin.publish.response({
-        data: { error, using: 'pushpin' },
-        channel
-      })
+      sendErrorResponse({ error, channel })
     }
   },
   auth: async (user, channel) => {
@@ -51,10 +62,33 @@ const userController = {
     } catch(error) {
       logger.error({ error }, 'An error has occurred trying to log the user in')
 
+      sendErrorResponse({ error, channel })
+    }
+  },
+  update: async ({ id, body, channel }) => {
+    if (_.isEmpty(body)) {
       pushpin.publish.response({
-        data: { error },
+        data: { message: 'Cannot update user with an empty body' },
         channel
       })
+    }
+
+    try {
+      const user = await checkIfUserExists({ id })
+      await validateUsername(body)
+      await validateEmail(body)
+      validatePassword(body)
+
+      const newUser = await user.update(body)
+
+      pushpin.publish.response({
+        data: { id, newUser },
+        channel
+      })
+    } catch(error) {
+      logger.error({ error }, 'An error has occurred trying to update user')
+
+      sendErrorResponse({ error, channel })
     }
   }
 }
--- src/router/private/index.js
+++ src/router/private/index.js
@@ -3,8 +3,16 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.status(200).json({ get: 'users' })
+const pushpin = require('../../controllers/pushpin')
+const userController = require('../../controllers/user')
+
+router.put('/:id', (req, res) => {
+  const { locals: { channel } } = res
+  const { body, params: { id } } = req
+
+  pushpin.sign.response({ res, channel })
+
+  userController.update({ id, body, channel })
 })
 
 module.exports = router
