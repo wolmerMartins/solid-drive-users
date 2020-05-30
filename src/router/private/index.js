1487f0dccc813eac061d5d081e09a04e0e0550b8
Wolmer Martins <wolmermartins@hotmail.com>
2020-05-30 07:59:51 -0300

    ENH: Creates the delete user route

--- src/router/private/index.js
+++ src/router/private/index.js
@@ -3,16 +3,26 @@
 const express = require('express')
 const router = express.Router()
 
+const verifyUser = require('../../middlewares/verifyUser')
+
 const pushpin = require('../../controllers/pushpin')
 const userController = require('../../controllers/user')
 
-router.put('/:id', (req, res) => {
-  const { locals: { channel } } = res
-  const { body, params: { id } } = req
+router.put('/:id', verifyUser, (req, res) => {
+  const { body } = req
+  const { locals: { user, channel } } = res
 
   pushpin.sign.response({ res, channel })
 
-  userController.update({ id, body, channel })
+  userController.update({ user, body, channel })
+})
+
+router.delete('/:id', verifyUser, (req, res) => {
+  const { locals: { user, channel } } = res
+
+  pushpin.sign.response({ res, channel })
+
+  userController.delete({ user, channel })
 })
 
 module.exports = router
