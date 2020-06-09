682875a7635051394eb0e747912e43b3cbf31161
Wolmer Martins <wolmermartins@hotmail.com>
2020-06-09 07:35:43 -0300

    ENH: Setup the mailer

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@
 const database = require('./config/database')
 const server = require('./config/server')
 const config = require('./config/config')
+const mailer = require('./config/mailer')
 const redis = require('./config/redis')
 const routes = require('./router')
 const logger = require('./logger')
 
 database(config.db)
 redis(config.redis)
+mailer(config.email)
 
 server.use(routes)
 
