10a3def05e8c9a1bb787596ffde909808e0a354d
Wolmer Martins <wolmermartins@hotmail.com>
2020-06-09 07:22:48 -0300

    REF: Changes the imports of tests

---
+++ src/controllers/tests/index.js
@@ -0,0 +1,11 @@
+'use strict'
+
+require('./jwt.test')
+require('./password.test')
+require('./pushpin.test')
+require('./utils.test')
+require('./validateActivateUser.test')
+require('./validateAuth.test')
+require('./validateLogin.test')
+require('./validateUser.test')
+require('./validationErrorSchema.test')
--- test/unit-tests/tests.js
+++ test/unit-tests/tests.js
@@ -1,17 +1,9 @@
 'use strict'
 
 describe('controllers', () => {
-  require('../../src/controllers/test/jwt.test')
-  require('../../src/controllers/test/utils.test')
-  require('../../src/controllers/test/pushpin.test')
-  require('../../src/controllers/test/password.test')
-  require('../../src/controllers/test/validateUser.test')
-  require('../../src/controllers/test/validateLogin.test')
-  require('../../src/controllers/test/validateAuth.test')
-  require('../../src/controllers/test/validationErrorSchema.test')
+  require('../../src/controllers/tests')
 })
 
 describe('models', () => {
-  require('../../src/models/test/User.test')
-  require('../../src/models/test/Session.test')
+  require('../../src/models/tests')
 })
