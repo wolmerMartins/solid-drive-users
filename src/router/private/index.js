8fa237dfdfb0b1f4db3254b615294d0a1cb5a992
Wolmer Martins <wolmermartins@hotmail.com>
2020-07-02 07:49:04 -0300

    ENH: Creates the logout route

--- src/router/private/index.js
+++ src/router/private/index.js
@@ -8,6 +8,8 @@ const verifyUser = require('../../middlewares/verifyUser')
 const pushpin = require('../../controllers/pushpin')
 const userController = require('../../controllers/user')
 
+const { COOKIE_KEY } = require('../../constants')
+
 router.put('/:id', verifyUser, (req, res) => {
   const { body } = req
   const { locals: { user, channel } } = res
@@ -23,6 +25,16 @@ router.delete('/:id', verifyUser, (req, res) => {
   pushpin.sign.response({ res, channel })
 
   userController.delete({ user, channel })
+})
+
+router.post('/logout', verifyUser, (req, res) => {
+  const { locals: { user, channel } } = res
+
+  res.clearCookie(COOKIE_KEY)
+
+  pushpin.sign.response({ res, channel })
+
+  userController.logout({ user, channel })
 })
 
 module.exports = router
